2979248d3a0ce295eca60b33736f5fee29ac6bf3
imam <a@gmail.com>
2024-03-21 15:51:07 +0530

    latest commit

--- variable.tf
+++ variable.tf
@@ -7,8 +7,3 @@ variable "ami" {
   description = "AMI ID for the EC2 instance"
   default     = "ami-08bca0a1996622bee"
 }
-
-variable "instance_type" {
-  description = "Instance type for the EC2 instance"
-  default     = "t2.micro"
-}
