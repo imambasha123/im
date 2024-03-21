b381b8c4e44567312413cd10c991f37fff4d68e3
imam <a@gmail.com>
2024-03-21 17:01:26 +0530

    latest commit

--- mai.tf
+++ mai.tf
@@ -1,12 +1,16 @@
-# main.tf
-
 provider "aws" {
   region = var.aws_region
 }
 
 resource "aws_instance" "example" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
+  ami              = var.ami_id
+  instance_type    = var.instance_type
+  key_name         = var.key_name
+  # Specify the default VPC ID
+  subnet_id        = aws_vpc.default.id
   # Add more configuration options as needed
+}
+
+data "aws_vpc" "default" {
+  default = true
 }
