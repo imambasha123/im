b4dc002eaefeed916092b39f44de18564d0a7a24
imam <a@gmail.com>
2024-03-21 15:43:26 +0530

    latest update

--- variable.tf
+++ variable.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "ami" {
   description = "AMI ID for the EC2 instance"
-  default     = "ami-026255a2746f88074"
+  default     = "ami-08bca0a1996622bee"
 }
 
 variable "instance_type" {
